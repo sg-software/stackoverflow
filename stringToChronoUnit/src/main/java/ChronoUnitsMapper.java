ede09545cac30038fdca5f766eb9bba278d2fe17
sg-software <susanne.gschwendner@gmail.com>
2021-11-01 22:43:26 +0100

    improve project structure

---
+++ stringToChronoUnit/src/main/java/ChronoUnitsMapper.java
@@ -0,0 +1,47 @@
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Showcases a possibility how chrono units can be extracted from a String
+ * with the help of regular expressions registered in a map and a function.
+ */
+public class ChronoUnitsMapper {
+
+    private static final Map<ChronoUnit,String> durationRegexMap = Map.ofEntries(
+            Map.entry(ChronoUnit.YEARS,"\\d+ (years|year)"),
+            Map.entry(ChronoUnit.MONTHS,"\\d+ (months|month)"),
+            Map.entry(ChronoUnit.DAYS, "\\d+ (days|day)")
+    );
+
+    public Map<ChronoUnit, Integer> parseDuration(String durationString) {
+        return new MapStringToChronoUnitsFunction(durationRegexMap)
+                .apply(durationString);
+    }
+
+    static class MapStringToChronoUnitsFunction implements Function<String, Map<ChronoUnit, Integer>> {
+
+        Map<ChronoUnit,String> durationRegexMap = new HashMap<>();
+
+        public MapStringToChronoUnitsFunction(Map<ChronoUnit,String> durationByRegex) {
+            durationRegexMap.putAll(durationByRegex);
+        }
+
+        @Override
+        public Map<ChronoUnit, Integer> apply(String textWithDurations) {
+            String[] splittedTextWithDurations = textWithDurations.split(",");
+
+            return this.durationRegexMap.entrySet().stream()
+                    .flatMap(regex -> Arrays.stream(splittedTextWithDurations)
+                            .map(String::trim)
+                            .filter(trimmedDurationString -> trimmedDurationString.matches(regex.getValue()))
+                            .map(matchingTrimmedDurationString -> matchingTrimmedDurationString.replaceAll("\\w+[a-zA-Z]", " "))
+                            .map(String::trim)
+                            .map(t -> Map.entry(regex.getKey(),Integer.valueOf(t))))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        }
+    }
+}
