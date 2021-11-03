9e328e841b23614dc66461b8fb26533a331c8da9
sg-software <susanne.gschwendner@gmail.com>
2021-11-03 22:00:32 +0100

    add spring stomp websocket tutorial app

---
+++ chatApp/src/main/java/com/example/messagingstompwebsocket/HelloMessage.java
@@ -0,0 +1,17 @@
+package com.example.messagingstompwebsocket;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
